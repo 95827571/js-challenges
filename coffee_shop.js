51bfb445bab6fc67ac224cd4d9dd69b599df4ccb
philip <philster36@hotmail.co.uk>
2023-01-27 15:38:43 +0000

    coffee shop code clean up

--- coffee_shop.js
+++ coffee_shop.js
@@ -2,7 +2,6 @@ const coffeeShop = {
     branch: ["Branch 1", "Branch 2", "Branch 3", "Branch 4"],
     food: [{item: "Cookies", cost: 2.25}, {item: "Oranges", cost: 1.00}, {item: "Banana", cost: 1.20}],
     drink: [{item: "Water", cost: 0.10}, {item: "Coca-Cola", cost: 1.50}, {item: "Pepsi", cost: 1.70}],
-
     drinksOrdered (amountOfItems) {
         let total = 0;
         let drinksString = "";
@@ -15,15 +14,13 @@ const coffeeShop = {
             drinksString += `\n${_drink["item"]}: ${_drink["cost"].toFixed(2)}`;
         }
 
-        let result = `${_branch} Order: ${drinksString}\nYour total cost is: ${total.toFixed(2)}`
-
-        return result
+        return `${_branch} Order: ${drinksString}\nYour total cost is: ${total.toFixed(2)}`
     },
     foodOrdered (amountOfItems) {
         let total = 0;
         let drinksString = "";
         let _branch = this.branch[Math.floor(Math.random()*this.branch.length)]
-        
+
         for (let amount = 0; amount < amountOfItems; amount++) {
             const _food = this.food[Math.floor(Math.random()*this.food.length)];
             total += _food["cost"];
@@ -31,11 +28,9 @@ const coffeeShop = {
             drinksString += `\n${_food["item"]}: ${_food["cost"].toFixed(2)}`;
         }
 
-        let result = `${_branch} Order: ${drinksString}\nYour total cost is: ${total.toFixed(2)}`
-
-        return result
+        return `${_branch} Order: ${drinksString}\nYour total cost is: ${total.toFixed(2)}`
     },
 }
 
 console.log(coffeeShop.drinksOrdered(2));
-console.log(coffeeShop.foodOrdered(2));
+console.log(coffeeShop.foodOrdered(4));
