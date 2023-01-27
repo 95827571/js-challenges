5ed268f555629b3a1cd6d80bbe2b78e010049311
philip <philster36@hotmail.co.uk>
2023-01-27 15:45:53 +0000

    coffee shop clean up

--- coffee_shop.js
+++ coffee_shop.js
@@ -1,34 +1,54 @@
+getRandomItemFromArray = (array) => {
+    return array[Math.floor(Math.random()*array.length)];
+}
+
 const coffeeShop = {
     branch: ["Branch 1", "Branch 2", "Branch 3", "Branch 4"],
     food: [{item: "Cookies", cost: 2.25}, {item: "Oranges", cost: 1.00}, {item: "Banana", cost: 1.20}],
     drink: [{item: "Water", cost: 0.10}, {item: "Coca-Cola", cost: 1.50}, {item: "Pepsi", cost: 1.70}],
+
     drinksOrdered (amountOfItems) {
         let total = 0;
         let drinksString = "";
-        let _branch = this.branch[Math.floor(Math.random()*this.branch.length)]
 
+        // Gets a random branch, not sure if this was the task
+        let _branch = getRandomItemFromArray(this.branch)
+
+        // Gets a random drink x amount of times
         for (let amount = 0; amount < amountOfItems; amount++) {
-            const _drink = this.drink[Math.floor(Math.random()*this.food.length)];
+
+            // Gets a random drink
+            const _drink = getRandomItemFromArray(this.drink);
+
+            // Adds the cost to a total
             total += _drink["cost"];
 
-            drinksString += `\n${_drink["item"]}: ${_drink["cost"].toFixed(2)}`;
+            // Concatenates to the end of a string
+            drinksString += `\n${_drink["item"]}: £${_drink["cost"].toFixed(2)}`;
         }
 
-        return `${_branch} Order: ${drinksString}\nYour total cost is: ${total.toFixed(2)}`
+        return `${_branch} Order: ${drinksString}\nYour total cost is: £${total.toFixed(2)}`
     },
     foodOrdered (amountOfItems) {
         let total = 0;
         let foodsString = "";
-        let _branch = this.branch[Math.floor(Math.random()*this.branch.length)]
 
+        // Gets a random branch
+        let _branch = getRandomItemFromArray(this.branch)
+
+        // Gets a random drink x amount of times
         for (let amount = 0; amount < amountOfItems; amount++) {
-            const _food = this.food[Math.floor(Math.random()*this.food.length)];
+            // Gets a random food item
+            const _food = getRandomItemFromArray(this.food);
+
+            // Adds to the total cost
             total += _food["cost"];
 
-            foodsString += `\n${_food["item"]}: ${_food["cost"].toFixed(2)}`;
+            // Concatenates to the end of a string
+            foodsString += `\n${_food["item"]}: £${_food["cost"].toFixed(2)}`;
         }
 
-        return `${_branch} Order: ${foodsString}\nYour total cost is: ${total.toFixed(2)}`
+        return `${_branch} Order: ${foodsString}\nYour total cost is: £${total.toFixed(2)}`
     },
 }
 
