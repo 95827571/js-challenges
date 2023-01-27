e86ce5f685438238c3a78965c1195f588b4d93ff
philip <philster36@hotmail.co.uk>
2023-01-27 14:39:59 +0000

    added objects

---
+++ objects.js
@@ -0,0 +1,23 @@
+// const pet = {
+//     name: "Best Hedgie",
+//     typeOfPet: "Hedgehog",
+//     colourOfPet: "Blue",
+//     age: 3,
+//     prettyAwesome: true,
+//     hobbies: ["Running fast", "Bungee Jumping", "Rock Climbing"], // I know, my hedgie is pretty advanced
+//     toString () {
+//         return `I have a ${this.typeOfPet}, called ${this.name} who is ${this.age} years old and is also ${this.colourOfPet}`;
+//     },
+//     checkIfAwesome () {
+//         if(this.prettyAwesome) {
+//             console.log(`${this.name} is pretty awesome`)
+//         } else {
+//             console.log(`${this.name} is not awesome, but if it's a hedgie(hedgehog) that's clearly a lie`)
+//         }
+//     }
+// }
+
+// console.log(pet.typeOfPet)
+// console.log(pet.name)
+// console.log(pet.toString()) // It stops on this line
+// console.log(pet.colourOfPet)
