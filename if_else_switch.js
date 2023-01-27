8bf4f1fbda905708cd4b05ca8df8f8181bc6131d
philip <philster36@hotmail.co.uk>
2023-01-27 14:39:35 +0000

    uncommented code

--- if_else_switch.js
+++ if_else_switch.js
@@ -1,231 +1,163 @@
-// let hunger = "full"
+let music = "asdad"
 
-// if(hunger == "hungry") {
-//     console.log("I am very hungry!");
-// } else if (hunger == "full") {
-//     console.log("I am bloody full.");
-// } else {
-//     console.log("Oh go on then, why not!")
-// }
+if (music.toLowerCase() == "classical") {
+    console.log("Oh, how relaxing")
+} else if (music.toLowerCase() == "disco") {
+    console.log("Where's the party?")
+} else {
+    console.log("Turn it down! 😒")
+}
 
-// let music = "asdad"
+let hunger = "hungry"
+let food = "sprouts"
 
-// if (music.toLowerCase() == "classical") {
-//     console.log("Oh, how relaxing")
-// } else if (music.toLowerCase() == "disco") {
-//     console.log("Where's the party?")
-// } else {
-//     console.log("Turn it down! 😒")
-// }
+if(hunger == "full" && food == "ice cream") {
+    console.log("Of course, who wouldn't")
+} else if (hunger == "hungry" && food == "ice cream") {
+    console.log("Yes please I'm straving, could I also have a pizza")
+} else if (hunger == "hungry" && food == "sprouts") {
+    console.log("Actually, I can wait")
+} else {
+    console.log("No thanks, I'm stuffed")
+}
 
-// if(1 === true) {
-//     console.log(true)
-// } else {
-//     console.log(false)
-// }
-
-
-// let age = 18
-
-// if (age > 17) {
-//     console.log("Yes I can serve you.")
-// } else {
-//     console.log("Sorry, I can't serve you.")
-// }
-
-// // let hunger = "hungry"
-// let food = "sprouts"
-
-// // if(hunger == "full" && food == "ice cream") {
-// //     console.log("Of course, who wouldn't")
-// // } else if (hunger == "hungry" && food == "ice cream") {
-// //     console.log("Yes please I'm straving, could I also have a pizza")
-// // } else if (hunger == "hungry" && food == "sprouts") {
-// //     console.log("Actually, I can wait")
-// // } else {
-// //     console.log("No thanks, I'm stuffed")
-// // }
-
-// if (food == "chips" || food == "pizza") {
-//     console.log("Yay lets eat!")
-// } else if (food == "broccoli" || food == "sprouts") {
-//     console.log("EWWW NO!")
-// } else {
-//     console.log("What should we eat?")
-// }
-// let day = "saturday"
-// if (day == "saturday" || day == "sunday") {
-//     console.log("Yay it's the weekend");
-// } else {
-//     console.log("Oh no, work again!")
-// }
-
-// let food = "sprouts"
-
-// switch(food) {
-//     case "sprouts":
-//         console.log("ew")
-//         break;
-//     case "pizza":
-//         console.log("yes")
-//         break;
-//     default:
-//         console.log("get me food")
-//         break;
-// }
-// let pizzaTopping = "pepperoni"
-
-// switch(pizzaTopping) {
-//     case "pepperoni":
-//         console.log(`My favourite topping is ${pizzaTopping}`);
-//         break;
-//     case "pineapple":
-//         console.log(`Are you crazy? ${pizzaTopping}!?!?!? 😒😒`)
-//         break;
-//     default:
-//         console.log("put a topping on it");
-//         break;
-// }
-
-// the classic fizz buzz
-// let result = ""
-// let num = 2
-// if (num % 3 == 0) {
-//     result += "fizz"
-// }
-// if (num % 5 == 0) {
-//     result += "buzz"
-// }
-// if (!(num % 5 == 0 || num % 3 == 0)) {
-//     result = num;
-// }
-// console.log(result)
-
+if (food == "chips" || food == "pizza") {
+    console.log("Yay lets eat!")
+} else if (food == "broccoli" || food == "sprouts") {
+    console.log("EWWW NO!")
+} else {
+    console.log("What should we eat?")
+}
+let day = "saturday"
+if (day == "saturday" || day == "sunday") {
+    console.log("Yay it's the weekend");
+} else {
+    console.log("Oh no, work again!")
+}
 
 // activity 2
-// let age = 17
+let drinkAge = 17
 
-// if (age >= 17) {
-//     console.log("I can serve you.")
-// } else {
-//     console.log("Sorry, I can not serve you.")
-// }
+if (drinkAge >= 17) {
+    console.log("I can serve you.")
+} else {
+    console.log("Sorry, I can not serve you.")
+}
 
 
-// // activity 3
-// let age = 16
-// let country = "UK"
-// if (age >= 17 && country === "UK") {
-//     console.log("I can serve you, are you sure you can afford with the cost of living?")
-// } else if (age >= 21 && country !== "UK") {
-//     console.log("I can serve you")
-// } else {
-//     console.log("I can't serve you")
-// }
+// activity 3
+let age = 16
+let country = "UK"
+if (age >= 17 && country === "UK") {
+    console.log("I can serve you, are you sure you can afford with the cost of living?")
+} else if (age >= 21 && country !== "UK") {
+    console.log("I can serve you")
+} else {
+    console.log("I can't serve you")
+}
 
-// // activity 4
-// let day = "saturday"
-// if(day == "saturday" || day == "sunday") {
-//     console.log("YAY ITS WEEKEND")
-// } else {
-//     console.log("IT'S NOT WEEKEND :(")
-// }
+// activity 4
+let weekDay = "saturday"
+if(weekDay == "saturday" || weekDay == "sunday") {
+    console.log("YAY ITS WEEKEND")
+} else {
+    console.log("IT'S NOT WEEKEND :(")
+}
 
-// // activity 4
-// let pizzaTopping = "pepperoni"
+// activity 4
+let pizzaTopping = "pepperoni"
 
-// switch(pizzaTopping) {
-//     case "pepperoni":
-//         console.log(`My favourite topping is ${pizzaTopping}`);
-//         break;
-//     case "pineapple":
-//         console.log(`Are you crazy? ${pizzaTopping}!?!?!? 😒😒`);
-//         break;
-//     default:
-//         console.log("put a topping on it");
-//         break;
+switch(pizzaTopping) {
+    case "pepperoni":
+        console.log(`My favourite topping is ${pizzaTopping}`);
+        break;
+    case "pineapple":
+        console.log(`Are you crazy? ${pizzaTopping}!?!?!? 😒😒`);
+        break;
+    default:
+        console.log("put a topping on it");
+        break;
         
-// }
+}
 
-// console.log(`${pizzaTopping}`)
+console.log(`${pizzaTopping}`)
 
-// // activity 5 
-// let password = "philip";
-// // philip is not short :()
-// if(password.length < 8) {
-//     console.log(`${password} is too short :(`);
-// } else {
-//     console.log(`${password} is strong enough!`);
-// }
+// activity 5 
+let password = "philip";
+if(password.length < 8) {
+    console.log(`${password} is too short :(`);
+} else {
+    console.log(`${password} is strong enough!`);
+}
 
-// // activity 6 - the classic fizz buzz
-// let result = ""
-// let num = 2
-// if (num % 3 == 0) {
-//     result += "fizz"
-// }
-// if (num % 5 == 0) {
-//     result += "buzz"
-// }
-// if (!(num % 5 == 0 || num % 3 == 0)) {
-//     result = num;
-// }
-// console.log(result)
+// activity 6 - the classic fizz buzz
+let fizzBuzzResult = ""
+let num = 2
+if (num % 3 == 0) {
+    fizzBuzzResult += "fizz"
+}
+if (num % 5 == 0) {
+    fizzBuzzResult += "buzz"
+}
+if (!(num % 5 == 0 || num % 3 == 0)) {
+    fizzBuzzResult = num;
+}
+console.log(fizzBuzzResult)
 
-// // activity 7
-// let townOfHome = "the UK, not sure where"
-// let placeOfWork = "Some development company"
-// let currentTime = 7;
+// activity 7
+let townOfHome = "the UK, not sure where"
+let placeOfWork = "Some development company"
+let currentTime = 7;
 
-// if(currentTime == 7) {
-//     console.log(`I'm at home in ${townOfHome}`);
-// } else if (currentTime == 8) {
-//     console.log("I am commuting");
-// } else if (currentTime == 9) {
-//     console.log(`I am at work in, ${placeOfWork}`);
-// }
+if(currentTime == 7) {
+    console.log(`I'm at home in ${townOfHome}`);
+} else if (currentTime == 8) {
+    console.log("I am commuting");
+} else if (currentTime == 9) {
+    console.log(`I am at work in, ${placeOfWork}`);
+}
 
 // activity 8
-// const stringToCheck = "jrfndklhgfndjkjlkgperfijfhdknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhejkdsoiufghedjwshi"
-// const vowels = ['a', 'e', 'i', 'o', 'u']
-// let result = ""
+const longString = "jrfndklhgfndjkjlkgperfijfhdknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhejkdsoiufghedjwshi"
+const vowels = ['a', 'e', 'i', 'o', 'u']
+let finalResult = ""
 
-// let stringArray = stringToCheck.split("").reverse();
+let stringArray = longString.split("").reverse();
 
-// for (let i = 0; i < stringArray.length; i++) {
-//     const element = stringArray[i];
-//     if (vowels.includes(element)) {
-//         result = element;
-//     }
-// }
+for (let i = 0; i < stringArray.length; i++) {
+    const element = stringArray[i];
+    if (vowels.includes(element)) {
+        result = element;
+    }
+}
 
-// console.log(result);
+console.log(result);
 
 // activity 9
-// const wordToCheck = "dead"
+const wordToCheck = "dead"
 
-// if(wordToCheck.charAt(0) === wordToCheck.charAt(wordToCheck.length-1)) {
-//     console.log("YAY")
-// } else {
-//     console.log(":(")
-// }
+if(wordToCheck.charAt(0) === wordToCheck.charAt(wordToCheck.length-1)) {
+    console.log("YAY")
+} else {
+    console.log(":(")
+}
 
 // activity 10
-// const givenNumber1 = 10
-// const givenNumber2 = 21
-// let result = givenNumber1 + givenNumber2;
-// if(result % 2 == 0) {
-//     console.log(result)
-// } else {
-//     result = givenNumber1 * givenNumber2;
-//     console.log(result);
-// }
-// function isEven(num1, num2) {
-//     return num1 + num2 % 2 == 0
-// }
+const givenNumber1 = 10
+const givenNumber2 = 21
+let result = givenNumber1 + givenNumber2;
+if(result % 2 == 0) {
+    console.log(result)
+} else {
+    result = givenNumber1 * givenNumber2;
+    console.log(result);
+}
+function isEven(num1, num2) {
+    return num1 + num2 % 2 == 0
+}
 
 // activity 11
-// const stringToCheck = 2020202
-// const reversedString = stringToCheck.toString().trim().split('').reverse().join('');
+const stringToCheck = 2020202
+const reversedString = stringToCheck.toString().trim().split('').reverse().join('');
 
-// console.log(`Is ${stringToCheck} Palindrome: ${stringToCheck == reversedString}`)
+console.log(`Is ${stringToCheck} Palindrome: ${stringToCheck == reversedString}`)
--- loops.js
+++ loops.js
@@ -4,33 +4,32 @@ let favouriteDrinks = [
     "Fanta"
 ]
 
-// for (let i = favouriteDrinks.length; i >= 0; i--) {
-//     console.log(favouriteDrinks[i]);
-// }
+for (let i = favouriteDrinks.length; i >= 0; i--) {
+    console.log(favouriteDrinks[i]);
+}
 
-// for (let i = 0; i < favouriteDrinks.length; i += 2) {
+for (let i = 0; i < favouriteDrinks.length; i += 2) {
     
-// }
+}
 
-// for loop :D
-// for (let i = 0; i < 5; i++) {
-//     console.log(`for loop index: ${i}`);
-// }
+for (let i = 0; i < 5; i++) {
+    console.log(`for loop index: ${i}`);
+}
 
 
-// // while loop :D
-// let index = 0
-// let keepGoing = true
-// while (true) {
-//     index++;
+// while loop :D
+let index = 0
+let keepGoing = true
+while (true) {
+    index++;
 
-//     console.log(`while loop index: ${index}`);
-//     if(index >= 11) {
-//         break;
-//     }
-// }
+    console.log(`while loop index: ${index}`);
+    if(index >= 11) {
+        break;
+    }
+}
 
-// // activity 2
+// activity 2
 let evenNumbers = []
 
 for (let i = 20; i > 0; i--) {
@@ -40,64 +39,64 @@ for (let i = 20; i > 0; i--) {
 }
 console.log(`The even numbers between 0 and 20 are; ${evenNumbers}`)
 
-// // activity 2 b
-// let oddNumbers = []
-// for (let i = 0; i < 30; i++) {
-//     if (i % 2 != 0) {
-//         oddNumbers.push(i)
-//     }
-// }
+// activity 2 b
+let oddNumbers = []
+for (let i = 0; i < 30; i++) {
+    if (i % 2 != 0) {
+        oddNumbers.push(i)
+    }
+}
 
-// console.log(`The odd numbers between 0 and 30 are: ${oddNumbers}`)
+console.log(`The odd numbers between 0 and 30 are: ${oddNumbers}`)
 
 // activity 3
-// let age = 1
-// while(age < 21) {
-//     age++;
-//     if (age < 18) {
-//         console.log("You're a child")
-//     } else {
-//         console.log("You're an adult")
-//     }
-// }
+let age = 1
+while(age < 21) {
+    age++;
+    if (age < 18) {
+        console.log("You're a child")
+    } else {
+        console.log("You're an adult")
+    }
+}
 
 // activity 4
-// let randomNumbers = [];
+let randomNumbers = [];
 
-// for (let i = 0; i < 6; i++) {
-//     let generatedNumber = Math.floor(Math.random()*6)+1;
-//     randomNumbers.push(generatedNumber);
-// }
+for (let i = 0; i < 6; i++) {
+    let generatedNumber = Math.floor(Math.random()*6)+1;
+    randomNumbers.push(generatedNumber);
+}
 
-// console.log(`My 6 random numbers are ${randomNumbers}`)
+console.log(`My 6 random numbers are ${randomNumbers}`)
 
 
 // activity 5
-// let filmsToCheck = [
-//     "Film 1",
-//     "Film 2",
-//     "Ghostbusters",
-//     "Film 3",
-// ]
+let filmsToCheck = [
+    "Film 1",
+    "Film 2",
+    "Ghostbusters",
+    "Film 3",
+]
 
-// for (let i = 0; i < filmsToCheck.length; i++) {
-//     const film = filmsToCheck[i];
-//     if (film.toLowerCase() == "ghostbusters") {
-//         console.log("YAY ITS GHOST BUSTERS!")
-//     } else {
-//         console.log("Boo! We want ghostbusters!")
-//     }
-// }
+for (let i = 0; i < filmsToCheck.length; i++) {
+    const film = filmsToCheck[i];
+    if (film.toLowerCase() == "ghostbusters") {
+        console.log("YAY ITS GHOST BUSTERS!")
+    } else {
+        console.log("Boo! We want ghostbusters!")
+    }
+}
 
 // activity 6
-// for (let i = 0; i < 6; i++) {
-//     let generatedNumber = Math.floor(Math.random()*30)
-//     if (generatedNumber % 7 == 0) {
-//         console.log(`${generatedNumber} is divisible by 7`)
-//     } else {
-//         console.log(`${generatedNumber} is not divisible by 7`)
-//     }
-// }
+for (let i = 0; i < 6; i++) {
+    let generatedNumber = Math.floor(Math.random()*30)
+    if (generatedNumber % 7 == 0) {
+        console.log(`${generatedNumber} is divisible by 7`)
+    } else {
+        console.log(`${generatedNumber} is not divisible by 7`)
+    }
+}
 
 // activity 7
 
