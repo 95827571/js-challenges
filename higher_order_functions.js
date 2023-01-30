4a8589234664b7bf3d80878733247200c3ecd3e0
philip <philster36@hotmail.co.uk>
2023-01-30 09:54:12 +0000

    higher order functions

---
+++ higher_order_functions.js
@@ -0,0 +1,15 @@
+// loops through each character of a string
+const forEachCharacter = (string, func) => {
+    for (let i = 0; i < string.length; i++) {
+        const element = string[i];
+        func(element);
+    }
+}
+
+// cast char to int
+const printCharAsASCIICode = (char) => {
+    console.log(char.charCodeAt(0));
+}
+
+forEachCharacter("hello", printCharAsASCIICode);
+forEachCharacter("Philip", (char) => {console.log(char)});
