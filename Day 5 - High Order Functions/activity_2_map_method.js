d333a0808dd2986936f27d616a5b242fc635ed6d
philip <86986414+95827571@users.noreply.github.com>
2023-01-31 12:50:15 +0000

    removed wrong comment

--- Day 5 - High Order Functions/activity_2_map_method.js
+++ Day 5 - High Order Functions/activity_2_map_method.js
@@ -1,6 +1,6 @@
 const ourNumbers = [5, 8, 1, 92, 5]
 
-// prints each char as its ascii code
+// multiplies each number by 3
 ourNumbers.map((num) => {
     const newNum = num * 3;
     console.log(`${num} * 3 = ${newNum}`);
